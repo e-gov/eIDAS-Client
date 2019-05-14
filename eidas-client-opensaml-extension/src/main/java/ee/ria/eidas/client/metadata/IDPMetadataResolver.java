2d2d6c8a199737fb6d5450d31aeca6cf9fa04cf1
priitr <priitr@gmail.com>
2019-05-14 15:36:09 +0300

    AUT-327 - update runtime dependencies to latest stable releases (#17)
    * AUT-327 - update runtime dependencies to latest stable releases
    
    * AUT-327 - update guava

--- eidas-client-opensaml-extension/src/main/java/ee/ria/eidas/client/metadata/IDPMetadataResolver.java
+++ eidas-client-opensaml-extension/src/main/java/ee/ria/eidas/client/metadata/IDPMetadataResolver.java
@@ -22,7 +22,7 @@ import org.opensaml.security.credential.CredentialSupport;
 import org.opensaml.security.credential.UsageType;
 import org.opensaml.security.credential.impl.StaticCredentialResolver;
 import org.opensaml.security.x509.X509Credential;
-import org.opensaml.xmlsec.config.DefaultSecurityConfigurationBootstrap;
+import org.opensaml.xmlsec.config.impl.DefaultSecurityConfigurationBootstrap;
 import org.opensaml.xmlsec.signature.impl.X509CertificateImpl;
 import org.opensaml.xmlsec.signature.support.impl.ExplicitKeySignatureTrustEngine;
 import org.slf4j.Logger;
