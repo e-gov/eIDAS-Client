e4355ff8c32fcd74a24926445979165342c5b6c7
Alar Kvell <alar.kvell@nortal.com>
2024-11-04 15:09:02 +0200

    AUT-1969 Add Elastic APM agent

--- eidas-client-webapp/src/main/java/ee/ria/eidas/client/webapp/EidasClientApplication.java
+++ eidas-client-webapp/src/main/java/ee/ria/eidas/client/webapp/EidasClientApplication.java
@@ -1,5 +1,6 @@
 package ee.ria.eidas.client.webapp;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -12,10 +13,12 @@ public class EidasClientApplication extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        ElasticApmAttacher.attach();
         return application.sources(EidasClientApplication.class);
     }
 
     public static void main(String[] args) {
+        ElasticApmAttacher.attach();
         SpringApplication.run(EidasClientApplication.class, args);
     }
 
